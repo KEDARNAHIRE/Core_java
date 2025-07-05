0da61e72b2617864c4c768a4617f67894cb57361
KEDARNAHIRE <kedarnahire.177@gmail.com>
2025-07-05 08:29:34 +0530

    Changes

--- String/Print_all_subString.java
+++ String/Print_all_subString.java
@@ -4,7 +4,7 @@ public class Print_all_subString {
     public static void main(String[] args) {
         String s = "abcd";
         for(int i=0;i<=3/*index is up to 3*/;i++){
-            for(int j=i+1;/*j is excluded*/j<=4;/*j is exclded*/j++){
+            for(int j=i+1;/*j is excluded*/j<=4;/*j is excluded*/j++){
                 System.out.print(s.substring(i,j)+" ");
             }
         }
